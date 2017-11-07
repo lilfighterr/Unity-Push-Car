280724a7804dec221a7840fd1bcc81d2c0fd5d50
DESKTOP-RT9K64F\Renz <lilfighter__@hotmail.com>
2017-11-06 17:59:51 -0700

    -Added Settings Scene -Did basic movement through scenes

--- Assets/Scripts/LoadScenes.cs
+++ Assets/Scripts/LoadScenes.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadScenes : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Main"); //Load scene
+    }
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene("Settings"); //Load scene
+    }
+    public void LoadCalibrate()
+    {
+        SceneManager.LoadScene("Calibrate");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
